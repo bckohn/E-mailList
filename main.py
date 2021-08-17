49a36294d4f7c58aa6ca16d8a8a9f0ae6f264ab4
Bruno Contel Kohn <bruno_kohn@hotmail.com>
2021-08-17 00:40:22 -0300

    Update main.py
    pequena atualização dos códigos, integração de teste e polimento dos comentários

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#func para formatar os nomes
+# func para formatar os nomes
 def create_mail(full_name, count, mail_created):
-
     interlist = []
 
     if len(full_name) < 3:
@@ -11,7 +10,6 @@ def create_mail(full_name, count, mail_created):
         interlist.append(full_name[-1].strip())
         mail_created.append(
             interlist[-1]+"."+full_name[0][0]+"."+full_name[1][0])
-
     return mail_created
 
 
@@ -24,39 +22,43 @@ lines = []
 compare_list = {}
 
 # operando o arquivo
-names_list = open("arqnomes.txt", "r")
-lines = names_list.readlines()
-names_list.close()
+try:
+    names_list = open("arqnomes.txt", "r")
+    lines = names_list.readlines()
+except OSError as err:
+    print("OS error: {0}".format(err))
+finally:
+    names_list.close()
 
 # loop para formatação dos nomes da lista
 try:
     for line in iter(lines):
-
         # separa os nomes
         auxiliar = line.split(" ")
-
         # chama função de formatação dos nomes
         mail_created = create_mail(auxiliar, int(len(auxiliar)), mail_created)
-
-except Exception as error:
-    print("Erro "+error+" na criação de e-mails")
+except:
+    print("Erro na formatação dos nomes")
 
 try:
-    # nomes para minúsculo
+    # nomes => lowercase
     for i in range(len(mail_created)):
         mail_created[i] = mail_created[i].lower()
 
-    # trackeando nomes repetidos
-    aux = 0
-    for i in mail_created:
-        if i in compare_list:
-            compare_list[i] += 1
-            mail_created[aux] = i + str(compare_list[i])
-        else:
-            compare_list[i] = 0
-        aux += 1
+    # trackeando e alterando nomes repetidos
+    try:
+        aux = 0
+        for i in mail_created:
+            if i in compare_list:
+                compare_list[i] += 1
+                mail_created[aux] = i + str(compare_list[i])
+            else:
+                compare_list[i] = 0
+            aux += 1
+    except:
+        print("Erro no tracking de duplicatas")
 
-    # adicionar o @ da empresa
+    # adicionar o domínio da empresa
     for i in range(len(mail_created)):
         mail_created[i] = str(mail_created[i]) + "@company.com"
 
@@ -65,12 +67,11 @@ try:
         auxlist = string.replace("-", "")
         final_list.append(auxlist)
 
-    # var count
+    # output do resultado dos e-mails
     count = 0
-
     for line in lines:
         print("<"+final_list[count]+"> "+line)
         count = count + 1
 
-except Exception as error:
-    print(error)
+except Exception as err:
+    print(err)
